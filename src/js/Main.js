d51c608bac59b124881ec16c7508e8a5199214bc
petriwna <petriwna43@gmail.com>
2025-04-29 12:55:27 +0300

    fix header and mobile

---
+++ src/js/Main.js
@@ -0,0 +1,28 @@
+export class Main {
+    constructor() {
+        this.mobileMenu = document.querySelector('.mobile-menu');
+        this.menuBtnOpen = document.querySelector('.menu-btn-open');
+        this.menuBtnClose = document.querySelector('.menu-btn-close');
+
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        this.menuBtnOpen.addEventListener('click', this.handleMenuButton.bind(this));
+        this.menuBtnClose.addEventListener('click', this.handleMenuButton.bind(this));
+    }
+
+
+    handleMenuButton() {
+        this.toggleMenu();
+        this.disableScroll();
+    }
+
+    toggleMenu() {
+        this.mobileMenu.classList.toggle('is-open');
+    }
+
+    disableScroll() {
+        document.body.classList.toggle('is-scroll-disable');
+    }
+}
