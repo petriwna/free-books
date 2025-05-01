7e1aa6d67dcd4573c86eb84017f2fb032fcbfbcf
petriwna <petriwna43@gmail.com>
2025-05-02 00:27:11 +0300

    fix

--- src/js/Main.js
+++ src/js/Main.js
@@ -4,6 +4,8 @@ export class Main {
         this.menuBtnOpen = document.querySelector('.menu-btn-open');
         this.menuBtnClose = document.querySelector('.menu-btn-close');
         this.form = document.querySelector('#get-form');
+        this.banner = document.querySelector('#cookie-banner');
+        this.acceptBtn = document.querySelector('#accept-cookies');
 
         this.addEventListeners();
     }
@@ -12,6 +14,15 @@ export class Main {
         this.menuBtnOpen.addEventListener('click', this.handleMenuButton.bind(this));
         this.menuBtnClose.addEventListener('click', this.handleMenuButton.bind(this));
         this.form.addEventListener('submit', this.handleFormSubmit.bind(this));
+
+        if (!localStorage.getItem('cookiesAccepted')) {
+            this.banner.style.display = 'flex';
+        }
+
+        this.acceptBtn.addEventListener('click', () => {
+            localStorage.setItem('cookiesAccepted', 'true');
+            this.banner.style.display = 'none';
+        });
     }
 
 
